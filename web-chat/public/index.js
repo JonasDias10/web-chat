c42489d7c5d73cc1dd366d4e1cb170b3919afdc1
Jonas Dias <zdias1413@gmail.com>
2023-10-08 22:59:48 -0300

    feat: add chat page files

---
+++ web-chat/public/index.js
@@ -0,0 +1,104 @@
+const URL = 'ws://localhost:8080';
+const socket = new WebSocket(URL);
+const messagesContainer = document.getElementById('chat-messages');
+
+const ACTIONS = {
+    CLIENTS_CONNECTED: 'clients_connected',
+    MESSAGE: 'message',
+    IMAGE: 'image'
+}
+
+document.getElementById('message-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const inputMessage = document.getElementById('message');
+    const message = inputMessage.value;
+    inputMessage.value = '';
+
+    const messageContainer = document.createElement('div');
+    messageContainer.classList.add('message', 'right-message');
+    messageContainer.innerText = message;
+    messagesContainer.appendChild(messageContainer);
+
+    handleSendMessage(message);
+});
+
+const handleSendMessage = (msg) => {
+    const message = {
+        action: ACTIONS.MESSAGE,
+        message: msg,
+        userName: sessionStorage.getItem('user-name')
+    }
+
+    socket.send(JSON.stringify(message));
+}
+
+socket.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data);
+
+    switch(data.action) {
+        case ACTIONS.CLIENTS_CONNECTED:
+            handleUpdateConnectedClients(data.count);
+            break;
+        case ACTIONS.MESSAGE:
+            handleReceiveMessage(data);
+            break;
+        default:
+            console.log('Error: Unknown action.');
+    }
+}); 
+
+const handleReceiveMessage = (message) => {
+    const messageContainer = document.createElement('div');
+    messageContainer.classList.add('message', 'left-message');
+
+    const span = document.createElement('span');
+    const p = document.createElement('p');
+
+    span.innerText = message.userName
+    p.innerText = message.message;
+
+    messageContainer.append(span, p);
+    messagesContainer.appendChild(messageContainer);
+};
+
+const handleUpdateConnectedClients = (connectedClients) => {
+    document.getElementById('online-count').innerText = connectedClients;
+};
+
+window.addEventListener('load', () => {
+    if (sessionStorage.getItem('user-name')) {
+        document.getElementById('welcome-chat').style.display = 'none';
+    }
+});
+
+document.getElementById('welcome-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const userName = document.getElementById('user-name').value;
+    sessionStorage.setItem('user-name', userName);
+
+    document.getElementById('welcome-chat').style.display = 'none';
+});
+
+/*
+const inputFile = document.getElementById('input-file');
+inputFile.addEventListener('change', (event) => {
+    handleUploadImage(event);
+});
+
+const handleUploadImage = (event) => {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(inputFile.files[0]);
+
+    reader.onload = (event) => {
+        const image = document.createElement('img');
+        image.setAttribute('src', event.target.result);
+    
+        messagesContainer.appendChild(image);
+    }
+
+};
+
+*/
